9df5b14942fbe133bc3ccd32e24a7749243e552d
jsdiazs <js.diazs@uniandes.edu.co>
2019-08-22 23:08:55 -0500

    revise

--- src/model/data_structures/Node.java
+++ src/model/data_structures/Node.java
@@ -3,25 +3,25 @@ package model.data_structures;
 public class Node<Item>
 {
 	private Item value;
-	
+
 	private Node<Item> next; 	
-	
+
 	public Node(Item pValue, Node<Item> pNext)
 	{
 		this.value = pValue;
 		this.next = pNext;
 	}
-	
+
 	public Item getValue()
 	{
 		return value;
 	}
-	
+
 	public void enlazarSiguiente(Node<Item> n)
 	{
 		next=n;
 	}
-	
+
 	public Node<Item> getNextNode()
 	{
 		return next;
--- src/model/logic/Travel.java
+++ src/model/logic/Travel.java
@@ -3,19 +3,19 @@ package model.logic;
 public class Travel 
 {
 	private int sourceID;
-	
+
 	private int dstID;
-	
+
 	private int month;
-	
+
 	private double mean_travel_time;
-	
+
 	private double standard_deviation_travel_time;
-	
+
 	private double  geometric_mean_travel_time;
-	
+
 	private double geometric_standard_deviation_travel_time;
-	
+
 	public Travel(int pSource, int pDstID, int pMonth, double pMeanTravelTime, double pStandartDeviationTravelTime, double pGeometricMeanTravelTime, double pGeometricStandart )
 	{
 		sourceID=pSource;
@@ -26,71 +26,71 @@ public class Travel
 		geometric_mean_travel_time=pGeometricMeanTravelTime;
 		geometric_standard_deviation_travel_time=pGeometricStandart;
 	}
-	
+
 	// Returns
-	
+
 	public int getSourceID()
 	{
 		return sourceID;
 	}
-	
+
 	public int getDstID()
 	{
 		return dstID;
 	}
-	
+
 	public int getMonth()
 	{
 		return month;
 	}
-	
+
 	public double getMean_travel_time()
 	{
 		return mean_travel_time;
 	}
-	
+
 	public double getStandard_deviation_travel_time()
 	{
 		return standard_deviation_travel_time;
 	}
-	
+
 	public double getGeometric_mean_travel_time()
 	{
 		return geometric_mean_travel_time;
 	}
-	
+
 	public double getGeometric_standard_deviation_travel_time()
 	{
 		return geometric_standard_deviation_travel_time;
 	}
-	
+
 	//Modificadores
-	
+
 	public void setSourceID(int pSourceID)
 	{
 		this.sourceID=pSourceID;
 	}
-	
+
 	public void setDstID(int pDstID)
 	{
 		this.dstID=pDstID;
 	}
-	
+
 	public void setMonth(int pMonth)
 	{
 		this.month=pMonth;
 	}
-	
+
 	public void setMean_travel_time(double pMean_travel_time)
 	{
 		this.mean_travel_time=pMean_travel_time;
 	}
-	
+
 	public void setStandard_deviation_travel_time(double pStandard_deviation_travel_time)
 	{
 		this.standard_deviation_travel_time=pStandard_deviation_travel_time;
 	}
-	
+
 	public void setGeometric_mean_travel_time(double pGeometric_mean_travel_time)
 	{
 		this.geometric_mean_travel_time=pGeometric_mean_travel_time;
